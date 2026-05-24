8ba28022f7048679bf12a528362caf52d9146504
Thushanika <thushanikaparamalingam02@gmail.com>
2026-05-24 19:37:34 +0530

    Student PUT route completed

--- app.py
+++ app.py
@@ -39,33 +39,40 @@ def create_student():
 
 
     if not data.get("full_name"):
+
         return jsonify({"ERROR": "Full name is required"}), 400
 
 
     if not data.get("email"):
+
         return jsonify({"ERROR": "Email is required"}), 400
 
 
-    existing_email = Student.query.filter_by(email=data["email"]).first()
+    existing_email = Student.query.filter_by(
+        email=data["email"]
+        ).first()
 
     if existing_email:
+
         return jsonify({"ERROR": "Email already exists"}), 400
 
 
     if not data.get("age"):
+
         return jsonify({"ERROR": "Age is required"}), 400
 
 
     if data["age"] <= 0:
+
         return jsonify({"ERROR": "Age must be a positive number"}), 400
 
 
     if not data.get("joined_date"):
+
         return jsonify({"ERROR": "Joined date is required"}), 400
 
 
     try:
-
         joined_date = datetime.strptime(data["joined_date"],"%Y-%m-%d").date()
 
     except:
@@ -75,6 +82,7 @@ def create_student():
     if "cgpa" in data:
 
         if data["cgpa"] < 0:
+
             return jsonify({"ERROR": "CGPA cannot be negative"}), 400
 
 
@@ -94,7 +102,9 @@ def create_student():
         return jsonify({"MESSAGE": "Created Student Successfully!"}), 201
 
     except Exception as e:
+
         db.session.rollback()
+
         return jsonify({"ERROR":"Internal server error","details":str(e)}), 500
     
 
@@ -123,6 +133,7 @@ def get_students():
         return jsonify(student_detail),200
     
     except Exception as e:
+
         return jsonify({"ERROR":"Internal server error","details":str(e)}), 500
 
 
@@ -136,6 +147,7 @@ def get_student(id):
         student=Student.query.get(id)
 
         if not student:
+            
             return jsonify({"ERROR": "Student is not found"}),400
 
         return jsonify({
@@ -150,6 +162,45 @@ def get_student(id):
         })
 
     except Exception as e:
+
+        return jsonify({"ERROR":"Internal server error","details":str(e)}), 500
+
+
+
+#UPDATE STUDENT BY ID
+
+@app.route('/api/students/<int:id>', methods=['PUT'])
+def update_student(id):
+
+    student = Student.query.get(id)
+
+    if not student:
+
+        return jsonify({'ERROR': 'Student is not found'}), 400
+
+    data = request.get_json()
+
+    if not data:
+
+        return jsonify({"ERROR":"Data is not found"}), 400
+
+    try:
+
+        student.full_name   = data.get('full_name',student.full_name)
+        student.email       = data.get('email', student.email)
+        student.age         = data.get('age', student.age)
+        student.cgpa        = data.get('cgpa', student.cgpa)
+        student.is_active   = data.get('is_active', student.is_active)
+        student.joined_date = data.get('joined_date', student.joined_date)
+
+        db.session.commit()
+
+        return jsonify({'message': 'Student updated successfully!'})
+
+    except Exception as e:
+
+        db.session.rollback()
+
         return jsonify({"ERROR":"Internal server error","details":str(e)}), 500
 
 
