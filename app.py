1fc263dda921dd4af3bd8a1bffbaea0c21b95042
Thushanika <thushanikaparamalingam02@gmail.com>
2026-05-24 13:19:12 +0530

    MySQL connection added

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
 app = Flask(__name__)
 
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root123@localhost/my_school"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
 @app.route("/")
 def home():
-    return "Hello!"
+    return "Flask + MySQL connected successfully!"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+if __name__ == '__main__':
+
+    try:
+        with app.app_context():
+            db.session.execute(text('SELECT 1'))
+            print("SUCCESS: Database connected successfully!")
+
+    except Exception as e:
+        print(f"ERROR: Database connection failed {e}")
+
+    app.run(debug=True, port=7777)
