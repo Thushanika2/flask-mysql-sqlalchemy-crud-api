4fb4a7e979f0ee0eba06022b1b97da1e13bfc002
Thushanika <thushanikaparamalingam02@gmail.com>
2026-05-26 14:58:18 +0530

    run.py updated

---
+++ run.py
@@ -0,0 +1,28 @@
+from sqlalchemy import text
+from app import create_app, db
+
+app = create_app()
+
+if __name__ == "__main__":
+
+    try:
+        with app.app_context():
+
+            db.session.execute(text("SELECT 1"))
+
+            print({"SUCCESS": "Database connected successfully!"})
+
+            try:
+                db.create_all()
+
+                print({"SUCCESS": "Tables created successfully!"})
+
+            except Exception as e:
+
+                print(f"ERROR: Table creation failed {e}")
+
+    except Exception as e:
+
+        print(f"ERROR: Database connection failed {e}")
+
+    app.run(debug=True, port=7777)
